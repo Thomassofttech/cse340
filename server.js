980bdeafeeeecbac394fcaadff9c3cc10cb53a86
thomassoftech <thomasgreat0@gmail.com>
2025-11-27 20:59:31 +0100

    week4

--- server.js
+++ server.js
@@ -12,10 +12,55 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require('./utilities/index')
+const accountRoute = require("./routes/accountRoute")
+const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
+/* ***********************
+ * Middleware
+ * Between the request and response
+ * ************************/
+// Unit 4, Sessions & Messages Activity
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+)
+// Unit 4, Sessions & Messages Activity
+// Express Messages Middleware
+app.use(require("connect-flash")())
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Unit 5 Authentication cookie use
+app.use(cookieParser())
 
 /* ***********************
  * View Engine And Templates
@@ -25,37 +70,19 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 
-/* ***********************
- * Middleware
- * ************************/
- app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  name: 'sessionId',
-}))
 
-// Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
 
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-//Index Route
+// Index route - Unit 3, Robust Error Handling activity
 app.get("/", utilities.handleErrors(baseController.buildHome))
-
-// Inventory routes
+// Inventory routes - Unit 3, Build Inventory route activity
 app.use("/inv", inventoryRoute)
+// Account routes - Unit 4, Deliver Login activity
+app.use("/account", accountRoute)
 
 
 
@@ -83,6 +110,9 @@ app.use(async (err, req, res, next) => {
     nav,
   })
 })
+
+
+
 
 /* ***********************
  * Local Server Information
